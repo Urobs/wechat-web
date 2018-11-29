049026de15d56713042a6b5e432da2942248bc1d
unknown <huangweiurob@gmail.com>
2018-11-30 01:02:59 +0800

    test

--- middleware/handle_msg.js
+++ middleware/handle_msg.js
@@ -1,18 +1,22 @@
 const msgProcess = require('../lib/message_process')
 
-module.exports = async function (ctx) {
-  const xmlMsg = ctx.request.body
-  res = xmlMsg.xml
-  const msgType = res.MsgType.toLowerCase()
-  if (msgProcess[msgType]) {
-    if (msgType === 'event') {
-      const event = res.Event.toLowerCase()
-      if (msgProcess[msgType][event]) { 
-        msgProcess[msgType][event](ctx, res)
+module.exports = function () {
+  return async function (ctx) {
+    const xmlMsg = ctx.request.body
+    res = xmlMsg.xml
+    const msgType = res.MsgType.toLowerCase()
+    if (msgProcess[msgType]) {
+      if (msgType === 'event') {
+        const event = res.Event.toLowerCase()
+        if (msgProcess[msgType][event]) { 
+          msgProcess[msgType][event](ctx, res)
+        }
       }
+      else {
+        msgProcess[msgType](ctx, res) 
+      }
+    } else {
+      msgProcess.replyDefault(ctx, res)
     }
-    else { msgProcess[msgType](ctx, res) }
-  } else {
-    msgProcess.replyDefault(ctx, res)
   }
 }
